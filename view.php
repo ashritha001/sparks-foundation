ca5cbba763123927864b5cd55f5a7d87f9e3e227
ashritha001 <55437646+ashritha001@users.noreply.github.com>
2021-07-18 13:43:39 +0530

    Add files via upload

---
+++ view.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DISPLAY CUSTOMER</title>
+  <div class="f">
+    <nav class="n">
+      <img src="b.jpg" class="na">
+<h2 class="logo">ASYA BANK</h2>
+
+ <a href="index.html" class="order1">HOME</a>
+ <a href="contact.php" class="order">CONTACT</a>
+
+</nav>
+</div>
+<style>
+  @media only screen and ( min-width: 100px)
+{
+    
+.order{
+    position: absolute;
+      top: 0cm;
+    font-size: 0.38cm;
+    left: 65%;
+  }
+  .order1{
+    position: absolute;
+    left: 90%;
+    top: 0cm;
+    font-size: 0.38cm;
+  }
+.order:hover{
+  opacity: 1;
+    padding: 0.1cm;
+    background-color: #DA70D6;
+    color: black;
+    top: -0.1cm;
+    border-radius: 0.2cm;
+    
+}
+.order1:hover{
+  opacity: 1;
+    top: -0.1cm;
+    padding: 0.1cm;
+    background-color: #DA70D6;
+    color: black;
+    border-radius: 0.2cm;
+    
+}
+
+  .n{
+    position: absolute;
+    top: 0.5cm;
+    right: 0.5cm;
+    height: 8%;
+    left: 0.5cm;
+
+    background-color: white;
+    
+  
+  }
+  .btn{
+  padding: 0.1cm;
+  background-color: purple;
+  font-size: 0.34cm;
+}
+.btn:hover{
+  background-color: green;
+    font-size: 0.44cm;
+}
+  .na{
+    width: 1cm;
+    height: 1cm;
+    position: absolute;
+    top: -0.3cm;
+    left: 0.2cm;
+  }
+  .n h2{
+    position: absolute;
+    font-size: 0.7cm;
+    left: 1.6cm;
+    top: -0.8cm;
+    background-color: white;
+    
+    color:#DA70D6;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  h2:hover{
+    text-decoration: underline;
+    color: black;
+  }
+  .to.active {
+  background-color: #5F9EA0;
+  color: white;
+}
+.tr{
+  padding: 0.1cm;
+  background-color: #9ee6d8;
+  color: #DA70D6;
+  margin-top: 2cm;
+}
+.tr:hover {
+  background-color: #5F9EA0;
+  color: black;
+}
+
+.tr.active {
+  background-color: #5F9EA0;
+  color: white;
+}
+table, th, td {
+  border: 1px solid black;
+}
+td{
+  color: purple;
+  text-align: center;
+  font-size: 0.3cm;
+}
+td:hover{
+  background-color:   #FF8C00;
+  color: black;
+  font-size: 0.35cm;
+}
+.a1{
+  position: absolute;
+  top: 2.5cm;
+  left:1%; 
+}
+th{
+ font-size: 0.25cm;
+
+}
+  .f{
+border: 2px solid black;
+position: absolute;
+top: 0cm;
+bottom: 17.2cm;
+left: 0.5cm;
+right: 0.5cm;
+  
+  }
+  .build{
+    background-image: url(mon1.jpeg);
+    background-size: cover;
+    width: 98%;
+    position: absolute;
+    top: 2cm;
+    border: 0.1cm solid #DA70D6;
+    left: 0.05cm;
+    width: 99%;
+   
+  height: 35cm
+
+  }
+}
+@media only screen and (min-width: 1000px){
+.n{
+    position: absolute;
+    top: 0.5cm;
+    right: 0.5cm;
+    height: 8%;
+    left: 0.5cm;
+
+    background-color: white;
+    }
+  .f{
+border: 2px solid black;
+position: absolute;
+top: 0cm;
+bottom: 17.2cm;
+left: 0.5cm;
+right: 0.5cm;
+  
+  }
+
+
+.topnav a {
+  float: right;
+  color: white;
+  text-align: center;
+  padding: 0.8cm 0.8cm;
+  padding-left: 0.6cm;
+  padding-right: 0.6cm;
+  text-decoration: none;
+  font-size: 17px;
+
+ }
+
+.topnav a:hover {
+  background-color:#DA70D6;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #DA70D6;
+  color: white;
+}
+.tp{
+  padding: 0.5cm;
+  background-color: #9ee6d8;
+  color: #455ca8;
+  margin-top: 2cm;
+}
+.tp:hover {
+  background-color: #5F9EA0;
+  color: black;
+}
+
+.tp.active {
+  background-color: #5F9EA0;
+  color: white;
+}
+.to{
+  padding: 0.5cm;
+  background-color: #9ee6d8;
+  color: #455ca8;
+  margin-top: 2cm;
+}
+.to:hover {
+  background-color: #5F9EA0;
+  color: black;
+}
+  .n h2{
+    position: relative;
+    left: 9%;
+    top: -0.7cm;
+    width: 7cm;
+    padding-left: 0.5cm;
+    background-color: white;
+    
+    color:#DA70D6;
+    font-weight: bold;
+    cursor: pointer;
+    font-size: 0.8cm;
+  }
+  h2:hover{
+    text-decoration: underline;
+    color: black;
+  }
+
+    .order{
+    position: absolute;
+    top: 18%;
+    font-size: 0.42cm;
+    left: 65%;
+  }
+  .order1{
+    position: absolute;
+    left: 85%;
+    top: 20%;
+    font-size: 0.42cm;
+  }
+.order:hover{
+  opacity: 1;
+    padding: 0.35cm;
+    top: -0.15cm;
+    background-color: #DA70D6;
+    color: black;
+    border-radius: 0.2cm;
+    
+}
+.order1:hover{
+  opacity: 1;
+    padding: 0.35cm;
+    top: -0.15cm;
+    background-color: #DA70D6;
+    color: black;
+    border-radius: 0.2cm;
+    
+}
+  .na{
+    width:2cm; 
+    height:1cm;
+    position: absolute;
+    top: 1%;
+    left: 3%;
+  }
+
+.to.active {
+  background-color: #5F9EA0;
+  color: white;
+}
+.tr{
+  padding: 0.5cm;
+  background-color: #9ee6d8;
+  color: #DA70D6;
+  margin-top: 2cm;
+}
+.tr:hover {
+  background-color: #5F9EA0;
+  color: black;
+}
+
+.tr.active {
+  background-color: #5F9EA0;
+  color: white;
+}
+table, th, td {
+  border: 1px solid black;
+}
+td{
+  color: purple;
+  text-align: center;
+  font-size: 0.5cm;
+}
+td:hover{
+  background-color:   #FF8C00;
+  color: black;
+  font-size: 0.6cm;
+}
+.a1{
+  position: absolute;
+  top: 2.5cm;
+  left:30%; 
+}
+
+  .build{
+    background-image: url(mon.jpg);
+    background-size: cover;
+    width: 98%;
+    position: absolute;
+    top: 2cm;
+    border: 0.1cm solid #DA70D6;
+    left: 0.05cm;
+    width: 99%;
+   
+  height: 35cm
+
+  }
+}
+</style>
+
+
+<body>
+  <center>
+<div class="build">
+</div>
+ <form method="POST" action="" enctype="multipart/form-data">
+
+<table class="a1">
+  <thead>
+    <tr>
+     <th>CUSTOMER ID</th>
+     <th>CUSTOMER NAME</th>
+     <th>CUSTOMER IMAGE</th>
+     <th>CUSTOMER EMAIL</th>
+     <th>PLACE</th>
+     <th>BALANCE</th>
+   
+</tr>
+  </thead>
+<?php
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "internship";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+  $query= "SELECT * from customer";
+  $query_run=mysqli_query($conn,$query);
+  while($row=mysqli_fetch_array($query_run))
+  {
+      ?>
+      <tr class="h">
+      <td><?php echo $row['customer_id']?></td>;
+        <td><?php echo $row['customer_name']?></td>;
+      <td><?php echo '<img src="data:image;base64,'.base64_encode($row['cust_image']).'" alt="image" style="width:100px;height:100px">';?></td>
+      <
+      <td><?php echo $row['customer_email']?></td>;
+      <td><?php echo $row['Place']?></td>;    
+<td><?php echo $row['balence']?></td>;  
+
+      </tr>
+      <?php
+  }
+  ?>
+</table>
+</form>
+</center>
+
+
+</body>
+</head>
+</html>
